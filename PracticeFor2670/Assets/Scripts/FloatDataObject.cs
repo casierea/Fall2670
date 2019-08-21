70f65877feb62877452a5b9fa23efa2fde1ccb59
Casie <dancingmongoose@gmail.com>
2019-08-21 14:36:19 -0600

    review for scripting. TriggerBehaviour. Ammo. Health

---
+++ PracticeFor2670/Assets/Scripts/FloatDataObject.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Globalization;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class FloatDataObject : ScriptableObject
+{
+	public float value = 1f;
+
+	public void UpdateValue(float number)
+	{
+		value += number;
+	}
+}
