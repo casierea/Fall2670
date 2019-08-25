b40065584c997ab3afc4ba90a260a6a6d4987a1d
Unknown <dancingmongoose@gmail.com>
2019-08-25 16:16:46 -0600

    Brackeys Tutorial Scriptable objects

---
+++ PracticeFor2670/Assets/Scripts/HomePractice/CardDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+
+	public CardTutScript card;
+	public Text nameText;
+	public Text descriptionText;
+	public Image artworkImage;
+
+	public Text manaText;
+	public Text attackText;
+	public Text healthText;
+	
+	
+	// Use this for initialization
+	void Start () {
+		//Debug.Log(card.name);
+		card.Print();
+	}
+	
+}
