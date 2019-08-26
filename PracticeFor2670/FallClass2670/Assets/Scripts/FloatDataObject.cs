730a0376d6b2cc75a547daf712ee8447d434efdf
Casie <dancingmongoose@gmail.com>
2019-08-26 13:56:07 -0600

    Script Transfer from other Unity Version

---
+++ PracticeFor2670/FallClass2670/Assets/Scripts/FloatDataObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class FloatDataObject : ScriptableObject
+{
+    public float value = 1f;
+
+    public void UpdateValue(float number)
+    {
+        value += number;
+    }
+}
---
+++ PracticeFor2670/FallClass2670/Assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Triggerbehaviour : MonoBehaviour
+{
+    public FloatDataObject data;
+    private void OnTriggerEnter(Collider other)
+    {
+        data.UpdateValue( -.1f);
+    }
+}
