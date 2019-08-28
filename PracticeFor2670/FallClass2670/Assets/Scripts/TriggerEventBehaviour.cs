a5dbe37aed82cfef34a90c4cb55d870c94bb9157
Casie <dancingmongoose@gmail.com>
2019-08-28 13:53:57 -0600

    TriggerStay,TriggerEnter

---
+++ PracticeFor2670/FallClass2670/Assets/Scripts/TriggerEventBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+   
+    public UnityEvent TriggerEnterEvent, TriggerStayEvent;
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStayEvent.Invoke();
+    }
+}
+
